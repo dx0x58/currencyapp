27b59f641dc32cdd499d32dbfed33303628b406a
Maxim Derbenev <maxim_ek@mail.ru>
2018-07-28 14:59:06 +0500

    add active scope to currency model

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,3 +1,5 @@
 class Currency < ApplicationRecord
   validates :value, presence: true, numericality: true, format: { with: /\A\d{1,3}(\.\d{0,4})?\z/ }
+
+  scope :active, -> { where(active: true) }
 end
