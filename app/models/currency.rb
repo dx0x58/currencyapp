b641aeffdf75b09653ef45d84211fbf84b5b3c01
Maxim Derbenev <maxim_ek@mail.ru>
2018-08-06 21:19:52 +0500

    add currency scopes + spec

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,5 +1,11 @@
 class Currency < ApplicationRecord
   validates :value, presence: true, numericality: true, format: { with: /\A\d{1,3}(\.\d{0,4})?\z/ }
 
-  scope :active, -> { find_by(active: true) }
+  def self.active
+    find_by(active: true)
+  end
+
+  def self.forced
+    active if active.try(:expiration_date).present?
+  end
 end
--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -6,4 +6,22 @@ describe Currency do
     it { is_expected.to validate_numericality_of(:value) }
     it { is_expected.to allow_value(111.2222).for(:value) }
   end
+
+  describe 'scopes' do
+    describe '.active' do
+      let!(:active_currency) { create :currency, :active }
+
+      it 'return active currency' do
+        expect(described_class.active).to eq active_currency
+      end
+    end
+
+    describe '.forced' do
+      let!(:forced_currency) { create :currency, :forced }
+
+      it 'return active currency' do
+        expect(described_class.forced).to eq forced_currency
+      end
+    end
+  end
 end
