212ab9f70dd1d38a0ce2e85cb1c1c15d1c821e70
Maxim Derbenev <maxim_ek@mail.ru>
2018-08-06 21:12:30 +0500

    refactor currency factory

--- spec/factories/currency.rb
+++ spec/factories/currency.rb
@@ -2,10 +2,21 @@ FactoryBot.define do
   factory :currency do
     value { rand(30.00..60.99) }
     active false
-    expiration_date { Time.current }
+    expiration_date nil
 
     trait :active do
       active true
     end
+
+    trait :forced do
+      active true
+      expiration_date { 1.hour.from_now }
+    end
+  end
+
+  factory :invalid_currency, class: 'Currency' do
+    value 'not_a_number'
+    active false
+    expiration_date nil
   end
 end
