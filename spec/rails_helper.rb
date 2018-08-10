a8ad8d3e4378c2cd444eadfe745a6ed4cbbc9285
Maxim Derbenev <maxim.ek1936@yandex.ru>
2018-08-10 18:16:41 +0500

    add system macros

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,6 +28,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include SystemMacros, type: :system
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
---
+++ spec/support/macros/system_macros.rb
@@ -0,0 +1,10 @@
+module SystemMacros
+  def fill_date_and_time(date, options = {})
+    field = options[:from]
+    select date.strftime('%Y'), from: "#{field}_1i" # year
+    select date.strftime('%B'), from: "#{field}_2i" # month
+    select date.strftime('%e').strip, from: "#{field}_3i" # day
+    select date.strftime('%H'), from: "#{field}_4i" # hour
+    select date.strftime('%M'), from: "#{field}_5i" # minute
+  end
+end
