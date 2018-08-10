ac4fe6670c98e468a58bd10ec9d4e3f6267c143d
Maxim Derbenev <maxim.ek1936@yandex.ru>
2018-08-10 18:15:51 +0500

    move spec config to config dir

--- spec/support/config/capybara.rb
+++ spec/support/config/capybara.rb
@@ -1,8 +1,3 @@
-# RSpec.configure do |config|
-#   Capybara.default_max_wait_time = 5
-#   Capybara.server = :puma
-# end
-
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
